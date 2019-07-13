38f9610049c347419c6498cbf11690b105ffc0c6
Semin Park <SPark9625@gmail.com>
2019-07-13 15:31:07 +0000

    Changed network architecture to DQN Nature

--- network.py
+++ network.py
@@ -4,30 +4,20 @@ class Policy(torch.nn.Module):
     def __init__(self, c_in, actions_out):
         super(Policy, self).__init__()
         self.conv = torch.nn.Sequential(
-            torch.nn.Conv2d(c_in, 32, 3, stride=2, padding=1, bias=False),
+            torch.nn.Conv2d(c_in, 32, 8, stride=4, padding=0, bias=False),
             torch.nn.ReLU(),
 
-            torch.nn.Conv2d(32, 32, 3, stride=2, padding=1, bias=False),
+            torch.nn.Conv2d(32, 64, 4, stride=2, padding=0, bias=False),
             torch.nn.ReLU(),
             
-            torch.nn.Conv2d(32,32, 3, stride=2, padding=1, bias=False),
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(32,32, 3, stride=2, padding=1, bias=False),
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(32,32, 3, stride=2, padding=1, bias=False),
+            torch.nn.Conv2d(64, 64, 3, stride=1, padding=0, bias=False),
             torch.nn.ReLU()
         )
 
-        # Assuming input size of 84x84, the output of the convolution becomes 32x3x3 = 288
-        self.fc = torch.nn.Sequential(
-            torch.nn.Linear(288, 256),
-            torch.nn.ReLU()
-        )
+        self.fc = torch.nn.Linear(3136, 512)
         
-        self.actor_fc = torch.nn.Linear(256, actions_out)
-        self.critic_fc = torch.nn.Linear(256, 1)
+        self.actor_fc = torch.nn.Linear(512, actions_out)
+        self.critic_fc = torch.nn.Linear(512, 1)
         
 
     def forward(self, x):
